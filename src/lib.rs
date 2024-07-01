3fd2089c07de6c01a354be25640e8fd1f591491f
HanGyeolee <46367614+HanGyeolee@users.noreply.github.com>
2024-07-01 17:45:35 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,11 @@ use crate::complie::compiler::{Compiler, MAGIC_NUMBER};
 use crate::interpreter::environment::Environment;
 use interpreter::Interpreter;
 use std::fmt;
-use rustc_hash::FxHashMap;
+use crate::ast::*;
 use crate::parser::parser::Parser;
 use crate::tokenizer::tokenizer::Tokenizer;
 use crate::virtualmachine::BytecodeEngine;
+use crate::vm::*;
 
 mod tokenizer;
 mod parser;
@@ -39,7 +40,7 @@ mod virtualmachine;
 
 
 /// # 인터프리터 엔진
-/// 쉬운 생성 및 쉬운 컴파일링 가능
+/// 쉬운 생성 및 쉬운 컴파일링이 가능합니다.
 pub struct InterustCompiler {
     interpreter: Interpreter,
     compiler: Compiler,
@@ -290,7 +291,7 @@ impl InterustCompiler {
 }
 
 /// # 가상 머신
-/// 컴파일된 파일 실행 및 외부에서 함수 및 변수를 호출 가능
+/// 컴파일된 파일 실행하고, 외부에서 함수 및 변수를 호출 가능합니다.
 pub struct InterustVM {
     bytecode_engine:BytecodeEngine,
 }
@@ -352,7 +353,7 @@ impl InterustVM{
 
     /// 외부에서 가상 머신 내에 존재하는 함수를 호출합니다.<br/>
     /// 함수는 가상 머신 내에서 동작하며 반환값이 없는 경우 None을 반환합니다.<br/>
-    /// 연산 중 오류가 발생하면, VMObject::Error() 를 반환합니다.
+    /// 연산 중 오류가 발생하거나 함수를 찾을 수 없으면, VMObject::Error() 를 반환합니다.
     /// # 매개변수
     /// - `name` : `String` 타입, 호출할 함수 식별자<br/>
     /// - `params` : `Vec<VMObejct>` 타입, 함수에 전달될 매개변수들<br/>
@@ -370,15 +371,17 @@ impl InterustVM{
     ///
     /// let mut interust_vm = InterustVM::new();
     /// interust_vm.import_compiled("test_export.irs");
-    /// let return_value = interust_vm.call_compiled_fn(String::from("add"), vec![]); // count:1
-    /// let return_value = interust_vm.call_compiled_fn(String::from("add"), vec![]); // count:2
+    /// let return_value = interust_vm.call_compiled_fn(String::from("add"), vec![]);
+    /// // count:1
+    /// let return_value = interust_vm.call_compiled_fn(String::from("add"), vec![]);
+    /// // count:2
     /// assert_eq!(return_value, None);
     /// ```
     pub fn call_compiled_fn(&mut self, name:String, params: Vec<VMObejct>) -> Option<VMObejct> {
         self.bytecode_engine.call_function(name, params)
     }
     /// 외부에서 가상 머신 내에 존재하는 변수를 호출합니다.<br/>
-    /// 연산 중 오류가 발생하면, VMObject::Error() 를 반환합니다.
+    /// 변수를 찾을 수 없으면, VMObject::Error() 를 반환합니다.
     /// # 매개변수
     /// - `name` : `String` 타입, 호출할 변수 식별자<br/>
     /// # 예제
@@ -396,7 +399,8 @@ impl InterustVM{
     ///
     /// let mut interust_vm = InterustVM::new();
     /// interust_vm.import_compiled("test_export.irs");
-    /// let return_value = interust_vm.call_compiled_fn(String::from("add"), vec![]); // count:1
+    /// let return_value = interust_vm.call_compiled_fn(String::from("add"), vec![]);
+    /// // count:1
     /// let value = interust_vm.call_compiled_var(String::from("count"));
     /// assert_eq!(value, VMObejct::I64(1));
     /// ```
@@ -423,7 +427,8 @@ impl InterustVM{
     ///
     /// let mut interust_vm = InterustVM::new();
     /// interust_vm.import_compiled("test_export.irs");
-    /// interust_vm.call_compiled_fn(String::from("add"), vec![]); // count:1
+    /// interust_vm.call_compiled_fn(String::from("add"), vec![]);
+    /// // count:1
     /// interust_vm.print();
     /// ```
     pub fn print(&self) {
@@ -683,6 +688,8 @@ mod test {
 }
 */
 
+pub type Program = Vec<Statement>;
+
 /// 인터프리터 해석기에서 토크나이징할 때 사용할 토큰들
 #[derive(Debug, PartialEq, Clone)]
 pub enum Token {
@@ -783,238 +790,246 @@ impl Object {
     }
 }
 
-pub type Program = Vec<Statement>;
+/// # 인터프리터에서 활용할 추상 구문 트리(AST)
+pub mod ast {
+    /// 추상 구문 상수
+    #[derive(Debug, PartialEq, Clone)]
+    pub enum Literal {
+        None,                   // 0x01
+        I64(i64),               // 0x02
+        F64(f64),               // 0x03
+        //u64                   // 0x04
+        //i32                   // 0x05
+        //f32                   // 0x06
+        //u32                   // 0x07
+        //i16                   // 0x08
+        //u16                   // 0x09
+        //i8                    // 0x0A
+        //u8                    // 0x0B
+        Bool(bool),             // 0x0C
+        //Vec                   // 0x0D
+        //Map                   // 0x0E
+        String(String),         // 0x0F
+    }
 
-/// 인터프리터 해석기에서 추상 구문 트리(AST)로 변환할 때 사용할 상수들
-#[derive(Debug, PartialEq, Clone)]
-pub enum Literal {
-   None,                   // 0x01
-   I64(i64),               // 0x02
-   F64(f64),               // 0x03
-   //u64                   // 0x04
-   //i32                   // 0x05
-   //f32                   // 0x06
-   //u32                   // 0x07
-   //i16                   // 0x08
-   //u16                   // 0x09
-   //i8                    // 0x0A
-   //u8                    // 0x0B
-   Bool(bool),             // 0x0C
-   //Vec                   // 0x0D
-   //Map                   // 0x0E
-   String(String),         // 0x0F
-}
-
-impl Literal{
-    pub fn get_type(&self) -> Type{
-        match self {
-            Literal::F64(_) => Type::F64,
-            Literal::I64(_) => Type::I64,
-            Literal::String(_) => Type::String,
-            Literal::Bool(_) => Type::Bool,
-            Literal::None => Type::None,
+    impl Literal{
+        pub fn get_type(&self) -> Type{
+            match self {
+                Literal::F64(_) => Type::F64,
+                Literal::I64(_) => Type::I64,
+                Literal::String(_) => Type::String,
+                Literal::Bool(_) => Type::Bool,
+                Literal::None => Type::None,
+            }
         }
     }
-}
 
-/// 인터프리터 해석기에서 추상 구문 트리(AST)로 변환할 때 사용할 타입들
-#[allow(non_camel_case_types)]
-#[repr(u8)]
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-pub enum Type {
-    None        =       0x71,
-    I64         =       0x72,
-    F64         =       0x73,
-    //u64       =       0x74,
-    //i32       =       0x75,
-    //f32       =       0x76,
-    //u32       =       0x77,
-    //i16       =       0x78,
-    //u16       =       0x79,
-    //i8        =       0x7A,
-    //u8        =       0x7B,
-    Bool        =       0x7C,
-    //Vec       =       0x7D,
-    //Map       =       0x7E,
-    String      =       0x7F,
-}
+    /// 추상 구문 타입
+    #[allow(non_camel_case_types)]
+    #[repr(u8)]
+    #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+    pub enum Type {
+        None        =       0x71,
+        I64         =       0x72,
+        F64         =       0x73,
+        //u64       =       0x74,
+        //i32       =       0x75,
+        //f32       =       0x76,
+        //u32       =       0x77,
+        //i16       =       0x78,
+        //u16       =       0x79,
+        //i8        =       0x7A,
+        //u8        =       0x7B,
+        Bool        =       0x7C,
+        //Vec       =       0x7D,
+        //Map       =       0x7E,
+        String      =       0x7F,
+    }
 
-impl std::fmt::Display for Type {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
-            Type::F64 => write!(f, "f64"),
-            Type::I64 => write!(f, "i64"),
-            Type::String => write!(f, "string"),
-            Type::Bool => write!(f, "bool"),
-            Type::None => write!(f, "var"),
+    impl std::fmt::Display for Type {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            match *self {
+                Type::F64 => write!(f, "f64"),
+                Type::I64 => write!(f, "i64"),
+                Type::String => write!(f, "string"),
+                Type::Bool => write!(f, "bool"),
+                Type::None => write!(f, "var"),
+            }
         }
     }
-}
 
-/// 인터프리터 해석기에서 추상 구문 트리(AST)로 변환할 때 사용할 연산들
-#[derive(Debug, PartialEq, Clone)]
-pub enum Infix {
-    Plus        = 0x20,     // +
-    Minus       = 0x21,     // -
-    Multiply    = 0x22,     // *
-    Divide      = 0x23,     // /
-    Mod         = 0x24,     // %
-    Equal       = 0x25,     // ==
-    NotEqual    = 0x26,     // !=
-    LessThan    = 0x27,     // <
-    GreaterThan = 0x28,     // >
-    And         = 0x29,     // &&
-    Or          = 0x2A,     // ||
-    BitAnd      = 0x2B,     // &
-    BitOr       = 0x2C,     // |
-}
+    /// 추상 구문 연산
+    #[derive(Debug, PartialEq, Clone)]
+    pub enum Infix {
+        Plus        = 0x20,     // +
+        Minus       = 0x21,     // -
+        Multiply    = 0x22,     // *
+        Divide      = 0x23,     // /
+        Mod         = 0x24,     // %
+        Equal       = 0x25,     // ==
+        NotEqual    = 0x26,     // !=
+        LessThan    = 0x27,     // <
+        GreaterThan = 0x28,     // >
+        And         = 0x29,     // &&
+        Or          = 0x2A,     // ||
+        BitAnd      = 0x2B,     // &
+        BitOr       = 0x2C,     // |
+    }
 
-impl std::fmt::Display for Infix {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
-            Infix::Plus => write!(f, "+"),
-            Infix::Minus => write!(f, "-"),
-            Infix::Multiply => write!(f, "*"),
-            Infix::Divide => write!(f, "/"),
-            Infix::Mod => write!(f, "%"),
-            Infix::Equal => write!(f, "=="),
-            Infix::NotEqual => write!(f, "!="),
-            Infix::LessThan => write!(f, "<"),
-            Infix::GreaterThan => write!(f, ">"),
-            Infix::And => write!(f, "&&"),
-            Infix::Or => write!(f, "||"),
-            Infix::BitAnd => write!(f, "&"),
-            Infix::BitOr => write!(f, "|"),
+    impl std::fmt::Display for Infix {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            match *self {
+                Infix::Plus => write!(f, "+"),
+                Infix::Minus => write!(f, "-"),
+                Infix::Multiply => write!(f, "*"),
+                Infix::Divide => write!(f, "/"),
+                Infix::Mod => write!(f, "%"),
+                Infix::Equal => write!(f, "=="),
+                Infix::NotEqual => write!(f, "!="),
+                Infix::LessThan => write!(f, "<"),
+                Infix::GreaterThan => write!(f, ">"),
+                Infix::And => write!(f, "&&"),
+                Infix::Or => write!(f, "||"),
+                Infix::BitAnd => write!(f, "&"),
+                Infix::BitOr => write!(f, "|"),
+            }
         }
     }
-}
 
-/// 인터프리터 해석기에서 추상 구문 트리(AST)로 변환할 때 사용할 접두연산들
-#[derive(Debug, PartialEq, Clone)]
-pub enum Prefix {
-    Minus       = 0x40,
-    Not         = 0x41,
-}
+    /// 추상 구문 접두연산
+    #[derive(Debug, PartialEq, Clone)]
+    pub enum Prefix {
+        Minus       = 0x40,
+        Not         = 0x41,
+    }
 
-/// 인터프리터 해석기에서 추상 구문 트리(AST)로 변환할 때 사용할 상태들
-#[derive(Debug, PartialEq, Clone)]
-pub enum Statement {
-    Let{                                // 0x50 from to[0x52 addr type]
+    /// 추상 구문 상태
+    #[derive(Debug, PartialEq, Clone)]
+    pub enum Statement {
+        Let{                                // 0x50 from to[0x52 addr type]
         variable: Expression,
-        expression: Option<Expression>
-    },
-    Return(Expression),                 // 0x51 exp
-    Expression(Expression),
-}
+            expression: Option<Expression>
+        },
+        Return(Expression),                 // 0x51 exp
+        Expression(Expression),
+    }
 
-/// 인터프리터 해석기에서 추상 구문 트리(AST)로 변환할 때 사용할 구문들
-#[derive(Debug, PartialEq, Clone)]
-pub enum Expression {
-    Variable(String, Type),             // 0x52 type
-    Identifier(String),                 // 변수 로드 = 0x53 addr
-    Insert {                            // 0x54 from to
+    /// 추상 구문 구문
+    #[derive(Debug, PartialEq, Clone)]
+    pub enum Expression {
+        Variable(String, Type),             // 0x52 type
+        Identifier(String),                 // 변수 로드 = 0x53 addr
+        Insert {                            // 0x54 from to
         variable:  Box<Expression>,
-        expression: Box<Expression>
-    },
-    If {                                // 0x55 cond cons_length alter_length cons (alter)
+            expression: Box<Expression>
+        },
+        If {                                // 0x55 cond cons_length alter_length cons (alter)
         condition: Box<Expression>,
-        consequence: Vec<Statement>,
-        alternative: Option<Vec<Statement>>,
-    },
-    Fn {                                // 0x56 return params_length body_size params[0x52 addr type, 0x52 addr type, ...] body
+            consequence: Vec<Statement>,
+            alternative: Option<Vec<Statement>>,
+        },
+        Fn {                                // 0x56 return params_length body_size params[0x52 addr type, 0x52 addr type, ...] body
         identifier: String,
-        return_type: Type,
-        parameters: Vec<Expression>,
-        body: Vec<Statement>,
-    },
-    Call {                              // 0x57 addr args_length args[0x53 index, 0x53 index, ...]
+            return_type: Type,
+            parameters: Vec<Expression>,
+            body: Vec<Statement>,
+        },
+        Call {                              // 0x57 addr args_length args[0x53 index, 0x53 index, ...]
         function: Box<Expression>,
-        arguments: Vec<Expression>,
-    },
+            arguments: Vec<Expression>,
+        },
 
-    Literal(Literal),
-    Prefix(Prefix, Box<Expression>),
-    Infix(
-        Infix,
-        Box<Expression>,
-        Box<Expression>
-    ),
+        Literal(Literal),
+        Prefix(Prefix, Box<Expression>),
+        Infix(
+            Infix,
+            Box<Expression>,
+            Box<Expression>
+        ),
+    }
+
+    /// 추상 구문 우선순위
+    #[derive(PartialEq, PartialOrd)]
+    pub enum Precedence {
+        Lowest,
+        Assign,         // =
+        Bool,           // && or ||
+        Equals,         // == or !=
+        LessGreater,    // > or <
+        Sum,            // +
+        Product,        // *
+        Bit,            // & or |
+        Prefix,         // -X or !X
+        Call,           // myFunction(X)
+    }
+
 }
 
-/// 인터프리터 해석기에서 추상 구문 트리(AST)로 변환할 때 사용할 연산 우선순위들
-#[derive(PartialEq, PartialOrd)]
-pub enum Precedence {
-    Lowest,
-    Assign,         // =
-    Bool,           // && or ||
-    Equals,         // == or !=
-    LessGreater,    // > or <
-    Sum,            // +
-    Product,        // *
-    Bit,            // & or |
-    Prefix,         // -X or !X
-    Call,           // myFunction(X)
-}
+/// # 가상 머신에서 활용할 열거자 및 테이블
+pub mod vm {
+    use std::fmt;
+    use rustc_hash::FxHashMap;
+    use crate::ast::Type;
 
+    /// 가상 머신 객체
+    #[derive(Debug, PartialEq, Clone)]
+    pub enum VMObejct {
+        Null,
+        I64(i64),
+        F64(f64),
+        Bool(bool),
+        String(String),
+        Fn(usize, u8, usize), // 함수의 시작 주소, 매개변수 개수, 바디의 크기
+        Error(String)
+    }
 
-/// VM 에서 사용할 객체들
-#[derive(Debug, PartialEq, Clone)]
-pub enum VMObejct {
-    Null,
-    I64(i64),
-    F64(f64),
-    Bool(bool),
-    String(String),
-    Fn(usize, u8, usize), // 함수의 시작 주소, 매개변수 개수, 바디의 크기
-    Error(String)
-}
-
-impl fmt::Display for VMObejct {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            VMObejct::F64(ref value) => write!(f, "{value}"),
-            VMObejct::I64(ref value) => write!(f, "{value}"),
-            VMObejct::Bool(ref value) => write!(f, "{value}"),
-            VMObejct::String(ref value) => write!(f, "{value}"),
-            VMObejct::Fn(ref addr, ref params_count, ref body_size) =>
-                write!(f, "fn {addr}:({params_count}) {{{body_size}}}"),
-            VMObejct::Null => write!(f, "null"),
-            VMObejct::Error(ref value) => write!(f, "{value}"),
+    impl fmt::Display for VMObejct {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match *self {
+                VMObejct::F64(ref value) => write!(f, "{value}"),
+                VMObejct::I64(ref value) => write!(f, "{value}"),
+                VMObejct::Bool(ref value) => write!(f, "{value}"),
+                VMObejct::String(ref value) => write!(f, "{value}"),
+                VMObejct::Fn(ref addr, ref params_count, ref body_size) =>
+                    write!(f, "fn {addr}:({params_count}) {{{body_size}}}"),
+                VMObejct::Null => write!(f, "null"),
+                VMObejct::Error(ref value) => write!(f, "{value}"),
+            }
         }
     }
-}
 
-/// VM 에서 사용할 상수들
-#[derive(Debug, PartialEq, Clone)]
-pub enum Constant {
-    None,                   // 0x01
-    I64(i64),               // 0x02
-    F64(f64),               // 0x03
-    //u64                   // 0x04
-    //i32                   // 0x05
-    //f32                   // 0x06
-    //u16                   // 0x07
-    //i16                   // 0x08
-    //u16                   // 0x09
-    //i8                    // 0x0A
-    //u8                    // 0x0B
-    Bool(bool),             // 0x0C
-    //Vec                   // 0x0D
-    //Map                   // 0x0E
-    String(String),         // 0x0F
-}
+    /// 가상 머신 상수
+    #[derive(Debug, PartialEq, Clone)]
+    pub enum Constant {
+        None,                   // 0x01
+        I64(i64),               // 0x02
+        F64(f64),               // 0x03
+        //u64                   // 0x04
+        //i32                   // 0x05
+        //f32                   // 0x06
+        //u16                   // 0x07
+        //i16                   // 0x08
+        //u16                   // 0x09
+        //i8                    // 0x0A
+        //u8                    // 0x0B
+        Bool(bool),             // 0x0C
+        //Vec                   // 0x0D
+        //Map                   // 0x0E
+        String(String),         // 0x0F
+    }
 
-/// VM에서 사용할 변수 혹은 함수 식별자를 담는 테이블
-#[derive(Debug, PartialEq, Clone)]
-pub struct Scope {
-    /// 변수 혹은 함수 식별자를 담는 테이블: 식별자 이름, (주소, 타입)
-    pub table: FxHashMap<String, (usize, Type)>,
-}
+    /// 가상 머신 테이블
+    #[derive(Debug, PartialEq, Clone)]
+    pub struct Scope {
+        /// 변수 혹은 함수 식별자를 담는 테이블: 식별자 이름, (주소, 타입)
+        pub table: FxHashMap<String, (usize, Type)>,
+    }
 
-impl Scope {
-    pub fn new() -> Self {
-        Scope {
-            table: FxHashMap::default(),
+    impl Scope {
+        pub fn new() -> Self {
+            Scope {
+                table: FxHashMap::default(),
+            }
         }
     }
 }
